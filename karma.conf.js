567fa9c64736f8931f80a2cc0e53a09e7784083b
Karlos g liberal (patxangas) <karlosgliberal@gmail.com>
2016-02-29 16:07:54 +0100

    add npm pre-commit
    pre-commit test
    
    pre-commit test
    
    test precommit
    
    test precommit
    
    test precommit
    
    test precommit
    
    test precommit
    
    test precommit
    
    test precommit
    
    test precommit
    
    test precommit
    
    test precommit

--- karma.conf.js
+++ karma.conf.js
@@ -1,89 +1,87 @@
 var path = require('path');
 
-module.exports = function(config) {
-    config.set({
+module.exports = function (config) {
+  config.set({
 
-        basePath: '',
+    basePath: '',
 
-        // use headless PhantomJS
-        browsers: ['PhantomJS2'],
+    // use headless PhantomJS
+    browsers: ['PhantomJS2'],
 
-        // use Jasmine with Sinon for mocking and stubs
-        frameworks: ['mocha', 'sinon-chai'],
+    // use Jasmine with Sinon for mocking and stubs
+    frameworks: ['mocha', 'sinon-chai'],
 
-        // load our single entry point for our tests
-        files: [
-            'test/index.js'
-        ],
+    // load our single entry point for our tests
+    files: [
+      'test/index.js'
+    ],
 
-        // preprocess with webpack and our sourcemap loader
-        preprocessors: {
-            'test/index.js': ['webpack']
-        },
+    // preprocess with webpack and our sourcemap loader
+    preprocessors: {
+      'test/index.js': ['webpack']
+    },
 
-        reporters: [
-            // https://github.com/mlex/karma-spec-reporter
-            'nyan',
+    reporters: [
+        // https://github.com/mlex/karma-spec-reporter
+      'nyan',
 
-            // https://github.com/karma-runner/karma-coverage
-            'coverage'
-        ],
-
-
-        // configure webpack within Karma
-        webpack: {
-
-            devtool: 'eval',
-
-            module: {
-                loaders: [{
-                    test: /\.spec\.js$/,
-                    loader: 'babel-loader',
-                    include: [path.resolve('src'), path.resolve('test')]
-                }, {
-                    test: /\.js$/,
-                    loader: 'isparta-loader',
-                    exclude: /\.spec\.js$/,
-                    include: [path.resolve('src'), path.resolve('test')]
-                }, {
-                    test: /\.html$/,
-                    loader: 'html'
-                }]
-            },
-            stats: {
-                colors: true
-            }
-        },
-
-        // Mocha Configurations
-        client: {
-            mocha: {
-                reporter: 'html' // change Karma's debug.html to the mocha web reporter
-            }
-        },
-
-        // configure the webpack server to not be so verbose
-        webpackServer: {
-            noInfo: true
-        },
-
-        webpackMiddleware: {
-            stats: {
-                colors: true,
-                chunks: false
-            }
-        },
-
-        // setup code coverage
-        coverageReporter: {
-            reporters: [{
-                type: 'text-summary',
-            }, {
-                type: 'html',
-                dir: 'coverage/'
-            }]
-        },
+        // https://github.com/karma-runner/karma-coverage
+      'coverage'
+    ],
 
 
-    });
+    // configure webpack within Karma
+    webpack: {
+
+      devtool: 'eval',
+
+      module: {
+        loaders: [{
+          test: /\.spec\.js$/,
+          loader: 'babel-loader',
+          include: [path.resolve('src'), path.resolve('test')]
+        }, {
+          test: /\.js$/,
+          loader: 'isparta-loader',
+          exclude: /\.spec\.js$/,
+          include: [path.resolve('src'), path.resolve('test')]
+        }, {
+          test: /\.html$/,
+          loader: 'html'
+        }]
+      },
+      stats: {
+        colors: true
+      }
+    },
+
+    // Mocha Configurations
+    client: {
+      mocha: {
+        reporter: 'html' // change Karma's debug.html to the mocha web reporter
+      }
+    },
+
+    // configure the webpack server to not be so verbose
+    webpackServer: {
+      noInfo: true
+    },
+
+    webpackMiddleware: {
+      stats: {
+        colors: true,
+        chunks: false
+      }
+    },
+
+    // setup code coverage
+    coverageReporter: {
+      reporters: [{
+        type: 'text-summary',
+      }, {
+        type: 'html',
+        dir: 'coverage/'
+      }]
+    },
+  });
 };
