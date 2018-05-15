742eb61c997a86d251a4e8c97cb6cd2f9e1177f5
mikelros <mikelros22@gmail.com>
2018-05-15 09:53:07 +0200

    Handle lopd errors and add descriptive error messages

--- src/app/views/logout/logout.controller.spec.js
+++ src/app/views/logout/logout.controller.spec.js
@@ -2,7 +2,7 @@ import logoutControllers from './logout.controllers';
 import localStorageManager from './../../services/localStorageManagerService/localStorageManager.services';
 
 describe('Logout controller', () => {
-  let $state, $translate, localStorageManagerStub, popupManagerStub, controller;
+  let $state, localStorageManagerStub, popupManagerStub, controller;
 
   beforeEach(angular.mock.module(logoutControllers.name));
 
@@ -10,13 +10,11 @@ describe('Logout controller', () => {
 
   beforeEach(inject(($controller) => {
     $state = {go: sinon.spy()};
-    $translate = {instant: sinon.spy()};
     localStorageManagerStub = {clearSession: sinon.spy()};
     popupManagerStub = {prompt: sinon.stub()};
 
     controller = $controller('LogoutController', {
       $state: $state,
-      $translate: $translate,
       localStorageManager: localStorageManagerStub,
       popupManager: popupManagerStub
     });
